f5075182f78a9ca74bd901714fbe168cb52e8513
artemats <artem.matseruk@gmail.com>
2022-06-17 09:46:05 +0300

    update values

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ const progress = document.querySelector('#progress');
 const currentValue = document.querySelector('#current-value');
 const leftValue = document.querySelector('#left-value');
 const goal = 58000;
-const current = 34037 + 16209;
+const current = 36537 + 16209;
 const percent = (( current / goal ) * 100).toFixed(0);
 
 /*
