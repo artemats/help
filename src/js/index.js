410064fb1c5ed125b32e987637186dc1da56e966
Artem <artem.matseruk@gmail.com>
2024-05-07 12:52:23 +0300

    update values

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ const progress = document.querySelector('#progress');
 const currentValue = document.querySelector('#current-value');
 const leftValue = document.querySelector('#left-value');
 const goal = 15000;
-const current = 5021;
+const current = 10036;
 const percent = (( current / goal ) * 100).toFixed(0);
 
 /*
